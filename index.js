619b3c25c19eb84af76e4043541e23748ca47270
Mr. W <mrw@vueform.com>
2023-10-25 18:08:27 +0200

    refactor: redo based on toggle-confirm example

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-import vueformPluginToggleHover from './src'
+import { toRefs, computed, onMounted, onBeforeUnmount } from 'vue'
+import './index.scss'
 
-export default vueformPluginToggleHover
+export default function vueformPluginToggleHover() {
+  return {
+    apply: 'ToggleElement',
+    props: {
+      hoverText: {
+        type: String,
+      },
+    },
+    setup(props, context, component) {
+      if (!props.hoverText) {
+        return {
+          ...component
+        }
+      }
+      
+      const {
+        hoverText: hoverContent,
+      } = toRefs(props)
+      
+      let $hoverDOM
+      let elInitialPositionProperty
+      
+      // ============== COMPUTED ==============
+      
+      const $el = computed(() => {
+        return component.input.value.$el
+      })
+      
+      // ============== METHODS ===============
+      
+      const handleMousenter = () => {
+        $hoverDOM = document.createElement('div')
+        $hoverDOM.setAttribute('data-vf-toggle-hover-wrapper', '')
+        $hoverDOM.innerHTML = hoverContent.value
+        
+        $el.value.append($hoverDOM)
+        $el.value.style.position = 'relative'
+      }
+      
+      const handleMouseleave = () => {
+        $hoverDOM.remove()
+        $el.value.style.position = elInitialPositionProperty
+      }
+      
+      // ================ HOOKS ===============
+      
+      onMounted(() => {
+        elInitialPositionProperty = $el.value.style.position
+        
+        $el.value.addEventListener('mouseenter', handleMousenter)
+        $el.value.addEventListener('mouseleave', handleMouseleave)
+      })
+      
+      onBeforeUnmount(() => {
+        $el.value.addEventListener('mouseenter', handleMousenter)
+        $el.value.addEventListener('mouseleave', handleMouseleave)
+      })
+      
+      
+      return {
+        ...component
+      }
+    }
+  }
+}

601dab9a8ddcdd9fd7896c607655fc6cd327ff94
Mr. W <mrw@vueform.com>
2023-11-03 12:37:48 +0100

    feat: css vars in template

--- TooltipModal.vue
+++ TooltipModal.vue
@@ -21,20 +21,14 @@ const props = defineProps({
 <style lang="scss">
 .vf-toggle-tooltip-modal {
   position: absolute;
-  bottom: 2.6rem;
-  
-  max-width: 400px;
-  width: max-content;
-  
-  min-height: 50px;
-  
-  padding: 1rem;
-  
-  background: #FFFFFF;
+  bottom: var(--vf-toggle-tooltip-modal-offset, 2.6rem);
+  max-width: var(--vf-toggle-tooltip-modal-max-width, 380px);
+  width: var(--vf-toggle-tooltip-modal-max-width, max-content);
+  padding: var(--vf-toggle-tooltip-modal-py, 1.5rem) var(--vf-toggle-tooltip-modal-px, 1.5rem);
+  background: var(--vf-toggle-tooltip-modal-bg, #FFFFFF);
+  box-shadow: var(--vf-toggle-tooltip-modal-shadow, 0 0 20px 5px #0000001a);
   pointer-events: none;
-  
   transform: translateX(calc(-50% + var(--vf-toggle-width) / 2));
-  box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.25);
 }
 
 .vf-toggle-tooltip-modal:after{
@@ -44,15 +38,27 @@ const props = defineProps({
   height: 0;
   bottom: -0.3rem;
   left: 50%;
-  box-sizing: border-box;
-  
-  border: 0.5rem solid #ffffff;
-  border-color: transparent transparent #ffffff #ffffff;
-  
   transform-origin: center;
   transform: rotateZ(-45deg) skew(-20deg, -20deg) translate(-50%, -50%);
-  
+  border: 0.5rem solid #ffffff;
+  border-color: transparent transparent #ffffff #ffffff;
   box-shadow: -10px 10px 14px 0px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
 }
 
+.vf-toggle-tooltip-title {
+  font-size: var(--vf-toggle-tooltip-modal-title-font-size, 18px);
+  line-height: var(--vf-toggle-tooltip-modal-title-line-height, 1.2);
+  font-weight: var(--vf-toggle-tooltip-modal-title-font-weight, 500);
+  color: var(--vf-toggle-tooltip-modal-title-color, inherit);
+  margin-bottom: var(--vf-toggle-tooltip-modal-title-mb, 0.75rem);
+  padding-bottom: var(--vf-toggle-tooltip-modal-title-pb, 0.75rem);
+  padding-right: var(--vf-toggle-tooltip-modal-close-width, 1.5rem);
+  border-bottom: var(--vf-toggle-tooltip-modal-title-border-width, 1px) solid var(--vf-toggle-tooltip-modal-title-border-color, var(--vf-gray-300));
+}
+
+.vf-toggle-tooltip-content {
+  font-size: var(--vf-toggle-tooltip-modal-font-size, 1rem);
+  color: var(--vf-toggle-tooltip-modal-color, inherit);
+}
 </style>
